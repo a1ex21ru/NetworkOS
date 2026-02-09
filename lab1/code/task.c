66c5259c5000f9b9d628171f54f6bfd4daa51c3a
alex21ru <r1nin@mail.ru>
2026-02-09 13:26:22 +0300

    lab 1 ready

--- lab1/code/task.c
+++ lab1/code/task.c
@@ -4,6 +4,7 @@
 #include <pthread.h>
 #include <unistd.h>
 #include <time.h>
+#include <linux/time.h>
 
 #define MAX_STREAK_FOR_STATE 5
 
@@ -28,10 +29,8 @@ struct Bathroom
     // сколько последовательно зашло человек одного пола
     unsigned streak;
     unsigned max_streak;
-
-    double total_busy_time;
-    struct timespec last_change;
 };
+typedef struct Bathroom Br;
 
 struct Student
 {
@@ -44,6 +43,10 @@ struct Student
     struct timespec enter;
     struct timespec leave;
 };
+
+typedef struct Student St;
+
+typedef struct timespec Time;
 
 const int BATHROOM_CAPACITY = 4;
 
@@ -58,89 +61,89 @@ double timespec_diff(struct timespec a, struct timespec b)
 }
 
 /// @brief Проверка доступности входа
-/// @param b - ванная
-/// @param s - студент
+/// @param b ванная
+/// @param s студент
 /// @return 
-bool canEnter(struct Bathroom* b, struct Student* s)
+bool canEnter(Br* b, St* s)
 {
     // все кабинки заняты
-    if (b->cabins_used == b->cabins_total)
-    {
+    if (b->cabins_used == b->cabins_total) {
         return false;
     }
 
     // если пусто, можно всем
-    if (b->state == nobody)
-    {
+    if (b->state == nobody) {
         return true;
     }
     
     // разный пол вместе находится не может
-    if (b->state != s->sex)
-    {
+    if (b->state != s->sex) {
         return false;
     }
 
     // Справедливый вход
-    if (b->streak >= MAX_STREAK_FOR_STATE) {
+    if (b->streak >= b->max_streak) {
+        // Приоритет противоположному полу, если они ждут
         if (s->sex == man && b->waiting_women > 0)
             return false;
 
         if (s->sex == woman && b->waiting_men > 0)
             return false;
-
     }
 
     return true;
 }
 
-
 /// @brief Вход в ванную
 /// @param b - ванная 
 /// @param s - студент
 /// @return 
-bool enterBathroom(struct Bathroom *b, struct Student *s)
+bool enterBathroom(Br *b, St *s)
 {
     pthread_mutex_lock(&b->mutex);
 
-    if (s->sex == man)
-    {
-        b->waiting_men++;
-    } else {
-        b->waiting_women++;
+    bool must_wait = !canEnter(b, s);
+
+    if (must_wait) {
+        if (s->sex == man) b->waiting_men++;
+        else b->waiting_women++;
     }
 
     while (!canEnter(b,s)) {
-
         // ожидание cond_broadcast
         pthread_cond_wait(&b->cond, &b->mutex);
     }
 
-    if (s->sex == man)
-    {
-        b->waiting_men--;
-    } else {
-        b->waiting_women--;
+    if (must_wait) {
+        if (s->sex == man) b->waiting_men--;
+        else b->waiting_women--;
     }
-
 
     if (b->state == nobody)
     {
         b->state = s->sex;
         b->streak = 0;
     }
+
     b->cabins_used++;
     b->streak++;
-
-    pthread_mutex_unlock(&b->mutex);
     
-    printf("%d.Student (%s) in, время (%f). Занято: %d/%d streak: %d\n",
+    printf("%4d. Student (%-5s) in, время (%5.3f). Занято: %d/%d streak: %d/%d \twait_m: %d wait_w: %d\n",
         s->studentID,
         s->sex == man ? "man":"woman",
         s->timeForShower,
         b->cabins_used, b->cabins_total,
-        b->streak
+        b->streak, b->max_streak,
+        b->waiting_men, b->waiting_women
     );
+
+    if (b->streak == b->max_streak) {
+        printf("\t>>> СМЕНА: Достигнут максимальный streak (%d) для %6s! <<<\n", 
+               b->max_streak,
+               s->sex == man ? "мужчин" : "женщин");
+    }
+    
+    pthread_mutex_unlock(&b->mutex);
 
     return true;
 }
@@ -149,7 +152,7 @@ bool enterBathroom(struct Bathroom *b, struct Student *s)
 /// @brief Выход из ванной
 /// @param b - ванная
 /// @param s - студент
-void leaveBathroom(struct Bathroom* b, struct Student* s) 
+void leaveBathroom(Br* b, St* s) 
 {
     pthread_mutex_lock(&b->mutex);
 
@@ -164,13 +167,14 @@ void leaveBathroom(struct Bathroom* b, struct Student* s)
     // посылаем сигнал всем: место освободилось, кто сможет тот зайдет
     pthread_cond_broadcast(&b->cond);
 
-    pthread_mutex_unlock(&b->mutex);
-
-    printf("%d.Student (%s) out. Занято: %d/%d\n",
+    
+    printf("%4d. Student (%-5s) out. Занято: %d/%d\n",
         s->studentID,
         s->sex == man ? "man":"woman",
         b->cabins_used, b->cabins_total
     );
+
+    pthread_mutex_unlock(&b->mutex);
 }
 
 struct Bathroom b = {
@@ -181,19 +185,19 @@ struct Bathroom b = {
     .cabins_used = 0,
     .state = nobody,
 
-    // .priority = nobody,
     .waiting_men = 0,
     .waiting_women = 0,
-    .streak = 5,
-};
+    .streak = 0,
+    .max_streak = MAX_STREAK_FOR_STATE,
 
+};
 
 /// @brief Функция потока студента
 /// @param arg 
 /// @return 
 void* studentThread (void* arg) {
 
-    struct Student* s = (struct Student*)arg;
+    St* s = (St*)arg;
 
     clock_gettime(CLOCK_MONOTONIC, &s->arrival);
     if (enterBathroom(&b,s)) {
@@ -210,13 +214,15 @@ void* studentThread (void* arg) {
     }
     clock_gettime(CLOCK_MONOTONIC, &s->leave);
 
-    pthread_exit(0);
+    return 0;
 }
 
 int initVarsFromCMD(int argc, char *argv[]);
 
 int random_number(int min_num, int max_num);
 
+//       8             4          5
+// students_count cabins_total streak
 int main(int argc, char *argv[]) {
 
     srand((unsigned)time(NULL));
@@ -226,7 +232,7 @@ int main(int argc, char *argv[]) {
         studLen = random_number(9, 25);
     }
 
-    struct Student students[100];
+    St students[100];
 
     int randTimeForShower;
     for (int i = 0; i < studLen; i++) {
@@ -237,42 +243,53 @@ int main(int argc, char *argv[]) {
             students[i].sex = woman;
         }
 
+        if (students[i].sex == woman) {
+            students[i].timeForShower = 2 * randTimeForShower;    
+        } else
+            students[i].timeForShower = randTimeForShower;
+
         students[i].studentID = i + 1;
-        students[i].timeForShower = randTimeForShower;
     }
+
+    printf("Начало: студентов - %d, кабинок - %d, max_streak - %d\n", studLen, b.cabins_total, b.max_streak);
 
     pthread_t tid[studLen];
 
-    for (int i = 0; i < studLen; i++) {
+    Time total_begin, total_end;
 
+    clock_gettime(CLOCK_MONOTONIC, &total_begin);
+    for (int i = 0; i < studLen; i++) {
         // создание потока
         pthread_create(&tid[i], NULL, studentThread, &students[i]);
     }
 
-    int resThread;
     for (int i = 0; i < studLen; i++) {
         // ожидание потока
-        resThread = pthread_join(tid[i], NULL);
-        if (resThread != 0) {
-            printf("Ошибка при завершении потока");
-        }
+        pthread_join(tid[i], NULL);
     }
+    printf("\t=======================Завершение======================\n");
 
-    double total_wait = 0.0;
+    double total_wait = 0.0, utilization = 0.0, total_time = 0.0, total_shower = 0.0;
 
     for (int i = 0; i < studLen; i++) {
         double wait = timespec_diff(students[i].arrival, students[i].enter);
-        printf("waiting time for student %d = %f\n", students[i].studentID, wait);
+        printf("Время ожидания для студента %d = %f\n", students[i].studentID, wait);
         total_wait += wait;
+        total_shower += timespec_diff(students[i].enter, students[i].leave);
     }
+    clock_gettime(CLOCK_MONOTONIC, &total_end);
+
+    total_time = timespec_diff(total_begin, total_end);
+
+    utilization = total_shower / (total_time * b.cabins_total) * 100;
 
     double avg_wait = total_wait / studLen;
-    printf("average time: %f", avg_wait);
+    printf("Среднее время ожидания: %f\n", avg_wait);
 
+    printf("Утилизация: %.2f%%\n", utilization);
 
     return 0;
 }
-
 
 int random_number(int min_num, int max_num) {
     int result = 0;
